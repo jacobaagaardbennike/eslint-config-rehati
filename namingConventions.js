a05a5df3fa9d078d14ecc6bd8cdc4f42daffee90
Jacob Ågård Bennike <jab@obton.com>
2024-02-23 14:27:11 +0100

    options for formatting updates

--- namingConventions.js
+++ namingConventions.js
@@ -2,22 +2,31 @@ const defaultOptions = [
   {
     selector: "default",
     format: ["strictCamelCase"],
-    leadingUnderscore: "allow",
-    trailingUnderscore: "allow",
-  },
-  {
-    selector: "import",
-    format: ["camelCase", "PascalCase"],
-  },
-  {
-    selector: "variable",
-    format: ["strictCamelCase", "UPPER_CASE"],
     leadingUnderscore: "forbid",
     trailingUnderscore: "forbid",
   },
   {
-    selector: "typeLike",
+    selector: [
+      "import",
+      "typeParameter",
+      "typeProperty",
+      "objectLiteralProperty",
+    ],
+    format: null,
+    leadingUnderscore: "allow",
+    trailingUnderscore: "allow",
+  },
+  {
+    selector: "variable",
+    format: ["strictCamelCase", "UPPER_CASE"],
+    leadingUnderscore: "allow",
+    trailingUnderscore: "forbid",
+  },
+  {
+    selector: ["class", "enum", "interface", "typeAlias"],
     format: ["StrictPascalCase"],
+    leadingUnderscore: "forbid",
+    trailingUnderscore: "forbid",
   },
 ];
 
